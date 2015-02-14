9aad176db34e58509cb9b71bacdc03e8c5fb6025
Mario Döring <deluna.yy@gmail.com>
2015-02-14 13:28:39 +0100

    Added line information to Lexer exception

--- src/Lexer.php
+++ src/Lexer.php
@@ -9,9 +9,9 @@
  * @copyright 		2015 ClanCats GmbH
  *
  */
- 
+
 use Hip\Lexer\Exception;
- 
+
 class Lexer
 {	
 	/**
@@ -20,52 +20,52 @@ class Lexer
 	 * @var string
 	 */
 	protected $code = null;
-	
+
 	/**
 	 * The code lenght to iterate
 	 *
 	 * @var int
 	 */
 	protected $length = 0;
-	
+
 	/**
 	 * The current string offset in the code
 	 *
 	 * @var int
 	 */
 	protected $offset = 0;
-	
+
 	/**
 	 * The current line
 	 *
 	 * @var int
 	 */
 	protected $line = 0;
-	
+
 	/**
 	 * Token map 
 	 *
 	 * @var array
 	 */
 	protected $tokenMap = array(
-	
+
 		// strings
 		'/^"[^"\\\\]*(?:\\\\.[^"\\\\]*)*"/' => 'string',
 		"/^'[^'\\\\]*(?:\\\\.[^'\\\\]*)*'/" => 'string',
-		
+
 		// numbers
 		"/^(([1-9]+\.[0-9]*)|([1-9]*\.[0-9]+)|([1-9]+))([eE][-+]?[0-9]+)?/" => 'number',
-		
+
 		// bool
 		"/^(yes)/" => "boolTrue",
 		"/^(no)/" => "boolFalse",
-		
+
 		// null
 		"/^(nil)/" => "null",
-		
+
 		// comments
 		"/^\#.*/" => "comment",
-		
+
 		//
 		"/^(\r\n|\n|\r)/" 				=> "linebreak",
 		"/^(\s)/" 						=> "whitespace",
@@ -73,7 +73,7 @@ class Lexer
 		"/^(:)/" 						=> "equal",
 		"/^(,)/" 						=> "comma",
 		"/^(\w+)/" 						=> "identifier",
-		
+
 	);
 
 	/**
@@ -86,11 +86,11 @@ class Lexer
 	{
 		// we have to convert all tabs to whitespaces to
 		$code = trim( str_replace( array( "  ", "\t" ), " ", $code ) );
-		
+
 		$this->code = $code;
 		$this->length = strlen( $code );
 	}
-	
+
 	/**
 	 * Get the codes lenght
 	 *
@@ -100,7 +100,7 @@ class Lexer
 	{
 		return $this->length;
 	}
-	
+
 	/**
 	 * Lex the next word
 	 * Return false everything has been parsed
@@ -113,7 +113,7 @@ class Lexer
 		{
 			return false;
 		}
-		
+
 		foreach( $this->tokenMap as $regex => $token ) 
 		{
 			if ( preg_match( $regex, substr( $this->code, $this->offset ), $matches ) ) 
@@ -122,16 +122,16 @@ class Lexer
 				{
 					$this->line++;
 				}
-				
+
 				$this->offset += strlen( $matches[0] );
-				
+
 				return new Token( array( $token, $matches[0], $this->line+1 ) );
 			}
 		}
-		
-		throw new Exception( sprintf( 'Unexpected character "%s"', $this->code[ $this->offset ] ) );
+
+		throw new Exception( sprintf( 'Unexpected character "%s" on line %s', $this->code[ $this->offset ], $this->line ) );
 	}
-	
+
 	/**
 	 * Lex the tokens from the code
 	 * 
@@ -141,12 +141,12 @@ class Lexer
 	public function tokens() 
 	{
 		$tokens = array();
-		
+
 		while( $token = $this->next() )
 		{
 			$tokens[] = $token;
 		}
-	
+
 		return $tokens;
 	}
 }
